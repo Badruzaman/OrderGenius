3a11e95a9b0c238700c8a8f555f64d3e1a079e06
badruzzaman <badruzzaman@CDK220900538>
2024-03-23 09:39:44 +0600

    added order services

---
+++ OrderGenius.Application/OrderServices/OrderService.cs
@@ -0,0 +1,31 @@
+﻿using OrderGenius.Core.Entities.OrderAggregate;
+using OrderGenius.Core.Interfaces;
+
+namespace OrderGenius.Application.OrderServices
+{
+    public class OrderService : IOrderService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public OrderService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<Order> CreateOrderAsync(Order order)
+        {
+            try
+            {
+                _unitOfWork.repository<Order>().Add(order);
+                var result = await _unitOfWork.Complete();
+                if (result <= 0)
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return order;
+        }
+    }
+}
---
+++ OrderGenius.Core/Interfaces/IOrderService.cs
@@ -0,0 +1,15 @@
+﻿using OrderGenius.Core.Entities.Identity;
+using OrderGenius.Core.Entities.OrderAggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderGenius.Core.Interfaces
+{
+    public interface IOrderService
+    {
+        Task<Order> CreateOrderAsync(Order order);
+    }
+}
